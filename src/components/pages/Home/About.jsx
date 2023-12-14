82aa8828d8bc917ac0d53f07d0d6fc5f2c84e3c5
Shaon22 <shaonroy11@gmail.com>
2023-12-14 23:09:04 +0600

    change some text

--- src/components/pages/Home/About.jsx
+++ src/components/pages/Home/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
             <h1 className='text-3xl font-bold text-center mt-10'>Introduction</h1>
             <div className="flex gap-10 my-20 items-center">
             <p className="text-lg text-gray-400 font-semibold">
-            Dive into our online library—a digital haven of endless books and resources. From bestsellers to academic archives, explore a boundless world of knowledge at your fingertips. No dusty shelves, just seamless access to a universe of information, all from the comfort of your screen.
+            Dive into our online library—a digital haven of endless books and resources. From bestsellers to academic archives, explore a boundless world of knowledge at your fingertips. No dusty shelves, just seamless access to a universe of information, all from the comfort of your.
             <button className="btn w-full text-white rounded bg-cyan-400 mt-3">Get Started With Us</button>
             </p>
             <img className="w-[800px]" src="https://i.ibb.co/hW2ZsDk/Car-Dealer-Hands-banner.jpg" alt="" />
